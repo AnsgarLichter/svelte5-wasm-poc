c6b17c169b44185b70872bb928e42eb506f6b9af
Ansgar Lichter <lichteransgar@gmail.com>
2024-07-10 22:38:18 +0200

    feat: ffmpeg video conversion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,175 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { tweened } from "svelte/motion";
+  import { fade } from "svelte/transition";
+
+  import { FFmpeg } from "@ffmpeg/ffmpeg";
+
+  type State =
+    | "loading"
+    | "loaded"
+    | "convert.start"
+    | "convert.done"
+    | "convert.error";
+
+  let state = $state("loading") as State;
+  let error = "";
+  let ffmpeg: FFmpeg;
+  let progress = tweened(0);
+
+  const loadFFmpeg = async () => {
+    const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+    ffmpeg = new FFmpeg();
+
+    ffmpeg.on('progress', (event: { progress: number; }) => {
+      progress.set(event.progress * 100);
+    });
+
+    state = "loading";
+    await ffmpeg.load({
+      coreURL: `${baseURL}/ffmpeg-core.js`,
+      wasmURL: `${baseURL}/ffmpeg-core.wasm`,
+    });
+    state = "loaded";
+  };
+
+  const fetchFile = async (file: File): Promise<Uint8Array> => {
+    return new Uint8Array(await file.arrayBuffer());
+  };
+
+  const convertVideo = async (video: File) => {
+    state = 'convert.start';
+    error = "";
+
+    const videoData = await fetchFile(video);
+    await ffmpeg.writeFile('input.webm', videoData);
+    await ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
+        
+    const data = await ffmpeg.readFile('output.mp4');
+    state = 'convert.done';
+
+    return data;
+  }
+
+  const download = (data: FileData) => {
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(new Blob([data], { type: 'video/mp4' }));
+    a.download = 'output.mp4';
+    a.click();
+  }
+
+  const onDropped = async (event: DragEvent) => {
+    event.preventDefault();
+
+    const files = event.dataTransfer?.files || [];
+    if (files.length === 0) {
+      return;
+    }
+
+    if (files.length > 1) {
+      error = "Only one file is allowed";
+      return;
+    }
+
+    const file = files[0];
+    if (file.type !== "video/webm") {
+      error = "Only WebM files are allowed";
+      return;
+    }
+
+    const data = await convertVideo(file);
+    download(data);
+  };
+
+  const onDraggedOver = (event: DragEvent) => {
+    event.preventDefault();
+  };
+
+  $effect(() => {
+    loadFFmpeg();
+  });
+</script>
+
+<h1 class="title">WebM to MP4 Converter</h1>
+
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div
+  ondrop={onDropped}
+  ondragover={onDraggedOver}
+  data-state={state}
+  class="drop"
+>
+  {#if state === "loading"}
+    <p in:fade>Loading FFMpeg ....</p>
+  {/if}
+
+  {#if state === "loaded"}
+    <p in:fade>Drag video here</p>
+  {/if}
+
+  {#if state === "convert.start"}
+    <p in:fade>Converting...</p>
+
+    <div class="progress-bar">
+      <div class="progress" style:--progress="{$progress}%">
+        {$progress.toFixed(0)}%
+      </div>
+    </div>
+  {/if}
+
+  {#if state === "convert.done"}
+    <p in:fade>Done!</p>
+  {/if}
+
+  {#if error}
+    <p in:fade class="error">{error}</p>
+  {/if}
+</div>
+
+<style>
+  .title {
+    text-align: center;
+  }
+
+  .drop {
+    width: 600px;
+    height: 400px;
+    display: grid;
+    place-content: center;
+    margin-block-start: 2rem;
+    border: 10px dashed green;
+    border-radius: 8px;
+
+    & p {
+      font-size: 2rem;
+      text-align: center;
+
+      &.error {
+        color: hsl(9 100% 64%);
+      }
+    }
+  }
+
+  .progress-bar {
+    --progress-bar-clr: hsl(180 100% 50%);
+    --progress-txt-clr: hsl(0 0% 0%);
+
+    width: 300px;
+    height: 40px;
+    position: relative;
+    font-weight: 700;
+    background-color: hsl(200 10% 14%);
+    border-radius: 8px;
+
+    & .progress {
+      width: var(--progress);
+      height: 100%;
+      position: absolute;
+      left: 0px;
+      display: grid;
+      place-content: center;
+      background: var(--progress-bar-clr);
+      color: var(--progress-txt-clr);
+      border-radius: 8px;
+    }
+  }
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+	optimizeDeps: {
+		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+	},
+	server: {
+		headers: {
+			'Cross-Origin-Opener-Policy': 'same-origin',
+			'Cross-Origin-Embedder-Policy': 'require-corp'
+		}
 	}
 });
